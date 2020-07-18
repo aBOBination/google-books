c8e2d4673ea4660cd29fa1180ce473c99a535758
Robert Marks <robert.marks54@gmail.com>
2020-07-18 15:02:51 -0600

    sync

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,9 @@
 const axios = require('axios');
 const router = require('express').Router();
 
-// router.get('/recipes', (req, res) => {
-//   axios
-//     .get('http://www.recipepuppy.com/api/', { params: req.query })
-//     .then(({ data: { results } }) => res.json(results))
-//     .catch((err) => res.status(422).json(err));
-// });
-
 router.get('/recipes', (req, res) => {
   axios
-    .get('https://www.googleapis.com/books/v1/volumes?q=HarryPotter')
+    .get('https://www.googleapis.com/books/v1/volumes?q=' + req.query.q)
     .then(({ data: { items } }) => {
       console.log(items);
       res.json(items);
